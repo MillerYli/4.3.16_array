92c8dc9594f0511865aff5fbdafe21f0b6dd678d
Yulia Miller <miller82.98@mail.ru>
2022-12-19 01:06:25 -0800

    Добавьте файлы проекта.

---
+++ 4.3.16 array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace mynamespase
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+            int count = 0;
+            
+            foreach(int i in arr)
+            {
+                if(i >= 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Кол-вот положительных элеменов: {0}", count);
+                
+               
+               
+           
+        }
+    }
+}
